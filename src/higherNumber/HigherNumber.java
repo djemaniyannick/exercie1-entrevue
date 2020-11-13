7552354ed28cd9078273d5b9937dbe9f8774bd2c
yannick <yanger842@gmail.com>
2020-11-13 09:42:03 -0500

    resolution exercice plus grand

---
+++ src/higherNumber/HigherNumber.java
@@ -0,0 +1,21 @@
+package higherNumber;
+
+import java.util.Scanner;
+
+public class HigherNumber {
+
+	public static void main(String[] args) {
+		
+		
+//demander a l'utilisation d'enter un nombre 
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("entrer a number :");
+		int number=sc.nextInt();
+		
+/*		converrtir le nombre en String puis en StringBuilder puis dans une boucle for comparer le dernier a l'avant dernier
+		si il est plus eleve permuter a la fin retransformer le nobre en int*/
+
+	}
+
+}
