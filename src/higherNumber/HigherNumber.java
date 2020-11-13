f3f5f126032eb271ff657a577eb3c30aaa8a9c6c
yannick <yanger842@gmail.com>
2020-11-13 09:52:57 -0500

    principe demander a l'utilisateur un nombre le transformer en string puis parcourir comparer le dernier et l'avant dernier nombre leur interchanger retransformer le nombre en int puis afficher le resultat

--- src/higherNumber/HigherNumber.java
+++ src/higherNumber/HigherNumber.java
@@ -15,7 +15,29 @@ public class HigherNumber {
 		
 /*		converrtir le nombre en String puis en StringBuilder puis dans une boucle for comparer le dernier a l'avant dernier
 		si il est plus eleve permuter a la fin retransformer le nobre en int*/
+		
+		
+		String numberToString=Integer.toString(number);
+		StringBuilder sb=new StringBuilder(numberToString);
+		int nextHigherNumber;
+		int len=numberToString.length();
+		char temp;
+		
+		for(int i=0 ; i<len-1;i++) {
+			if(sb.charAt(len-1-i)> sb.charAt(len-2-i)) {
+				temp=sb.charAt(len-1-i);
+				  sb.setCharAt(len-1-i,sb.charAt(len-2-i));
+			      sb.setCharAt(len-2-i,temp);
+			      break;
+			}
+			
+		}
+		nextHigherNumber=Integer.parseInt(sb.toString());
+		System.out.println(" le nombre superieur  a "+number+" en utilisant le meme nombre est : "+nextHigherNumber);
+
 
 	}
+	
+	
 
 }
